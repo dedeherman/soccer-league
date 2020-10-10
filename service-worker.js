5d6c454415afceea9bf4fd08a4c0b350a3b6cad2
Dede Herman <dedehermanvpay@gmail.com>
2020-10-10 17:52:32 +0700

    edit content

--- service-worker.js
+++ service-worker.js
@@ -1,66 +1,66 @@
 const CACHE_NAME = "firstpwa";
 var urlsToCache = [
-  "/",
-  "/nav.html",
-  "/index.html",
-  "/pages/home.html",
-  "/pages/liga-champion.html",
-  "/pages/liga-indonesia.html",
-  "/pages/liga-inggris.html",
-  "/pages/liga-italia.html",
-  "/css/materialize.min.css",
-  "/css/materialize.css",
-  "/css/style.css",
-  "/js/materialize.min.js",
-  "/js/nav.js",
-  "/js/materialize.js",
-  "/manifest.json",
-  "/icon.png",
-  "/favicon.ico",
-  "/image/liga-italia.jpg",
-  "/image/liga-champion.jpg",
-  "/image/liga-indonesia.jpg",
-  "/image/liga-inggris.jpg"
+    "/",
+    "/nav.html",
+    "/index.html",
+    "/pages/home.html",
+    "/pages/liga-champion.html",
+    "/pages/liga-indonesia.html",
+    "/pages/liga-inggris.html",
+    "/pages/liga-italia.html",
+    "/css/materialize.min.css",
+    "/css/materialize.css",
+    "/css/style.css",
+    "/js/materialize.min.js",
+    "/js/nav.js",
+    "/js/materialize.js",
+    "/manifest.json",
+    "/icon.png",
+    "/favicon.ico",
+    "/image/liga-italia.jpg",
+    "/image/liga-champion.jpg",
+    "/image/liga-indonesia.jpg",
+    "/image/liga-inggris.jpg"
 ];
 
-self.addEventListener("install", function (event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(function (cache) {
-      return cache.addAll(urlsToCache);
-    })
-  );
-});
-
-self.addEventListener("fetch", function (event) {
-  event.respondWith(
-    caches
-      .match(event.request, { cacheName: CACHE_NAME })
-      .then(function (response) {
-        if (response) {
-          console.log("ServiceWorker: Gunakan aset dari cache: ", response.url);
-          return response;
-        }
-
-        console.log(
-          "ServiceWorker: Memuat aset dari server: ",
-          event.request.url
-        );
-        return fetch(event.request);
-      })
-  );
-});
-
-self.addEventListener("activate", function (event) {
-  event.waitUntil(
-    caches.keys().then(function (cacheNames) {
-      return Promise.all(
-        cacheNames.map(function (cacheName) {
-          if (cacheName != CACHE_NAME) {
-            console.log("ServiceWorker: cache " + cacheName + " dihapus");
-            return caches.delete(cacheName);
-          }
+self.addEventListener("install", function(event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(function(cache) {
+            return cache.addAll(urlsToCache);
         })
-      );
-    })
-  );
+    );
+});
+
+self.addEventListener("fetch", function(event) {
+    event.respondWith(
+        caches
+        .match(event.request, { cacheName: CACHE_NAME })
+        .then(function(response) {
+            if (response) {
+                console.log("ServiceWorker: Gunakan aset dari cache: ", response.url);
+                return response;
+            }
+
+            console.log(
+                "ServiceWorker: Memuat aset dari server: ",
+                event.request.url
+            );
+            return fetch(event.request);
+        })
+    );
+});
+
+self.addEventListener("activate", function(event) {
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.map(function(cacheName) {
+                    if (cacheName != CACHE_NAME) {
+                        console.log("ServiceWorker: cache " + cacheName + " dihapus");
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
 });
