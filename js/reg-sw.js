42f69906b1a8c8329323f8606d75d76e9b36ee66
dedeherman <dedehermanvpay@gmail.com>
2020-11-18 18:35:58 +0700

    edit

--- js/reg-sw.js
+++ js/reg-sw.js
@@ -1,15 +1,75 @@
-// REGISTER SERVICE WORKER
-if ("serviceWorker" in navigator) {
-    window.addEventListener("load", function() {
-        navigator.serviceWorker
-            .register("/service-worker.js")
-            .then(function() {
-                console.log("Pendaftaran ServiceWorker berhasil");
-            })
-            .catch(function() {
-                console.log("Pendaftaran ServiceWorker gagal");
-            });
-    });
+// Periksa service worker
+if (!('serviceWorker' in navigator)) {
+    console.log("Service worker tidak didukung browser ini.");
 } else {
-    console.log("ServiceWorker belum didukung browser ini.");
+    registerServiceWorker();
+}
+// Register service worker
+function registerServiceWorker() {
+    return navigator.serviceWorker.register('service-worker.js')
+        .then(function(registration) {
+            console.log('Registrasi service worker berhasil.');
+            return registration;
+        })
+        .catch(function(err) {
+            console.error('Registrasi service worker gagal.', err);
+        });
+}
+
+// Periksa fitur Notification API
+if ("Notification" in window) {
+    requestPermission();
+} else {
+    console.error("Browser tidak mendukung notifikasi.");
+}
+
+// Meminta ijin menggunakan Notification API
+function requestPermission() {
+    if ('Notification' in window) {
+        Notification.requestPermission().then(result => {
+            if (result === "denied") {
+                console.log("Fitur notifikasi tidak diijinkan.");
+                return;
+            } else if (result === "default") {
+                console.error("Pengguna menutup kotak dialog permintaan ijin.");
+                return;
+            }
+
+            if (('PushManager' in window)) {
+                // Jika service worker sudah ready (navigator.serviceWorker.ready)
+                navigator.serviceWorker.ready.then(reg => {
+                    reg.pushManager.subscribe({
+                        userVisibleOnly: true,
+                        applicationServerKey: urlBase64ToUint8Array("BAhp35Ni4oAL2nqH6qei07lIutCE9WX5u-3fTeLNhYbh9e4hrER6QgGPD8a6a7Pz_61t6hYifaXGptkMQVpRCtg")
+                    }).then(sub => {
+                        console.log('Berhasil melakukan subscribe dengan endpoint: ', sub.endpoint);
+                        console.log('Berhasil melakukan subscribe dengan p256dh key: ', btoa(String.fromCharCode.apply(
+                            null, new Uint8Array(sub.getKey('p256dh')))));
+                        console.log('Berhasil melakukan subscribe dengan auth key: ', btoa(String.fromCharCode.apply(
+                            null, new Uint8Array(sub.getKey('auth')))));
+                    }).catch(e => {
+                        console.error('Tidak dapat melakukan subscribe ', e);
+                        // kembalikan agar menjalankan register service worker
+                        return registerServiceWorker();
+                    });
+
+                });
+            }
+
+
+        });
+    }
+}
+
+function urlBase64ToUint8Array(base64String) {
+    const padding = '='.repeat((4 - base64String.length % 4) % 4);
+    const base64 = (base64String + padding)
+        .replace(/-/g, '+')
+        .replace(/_/g, '/');
+    const rawData = window.atob(base64);
+    const outputArray = new Uint8Array(rawData.length);
+    for (let i = 0; i < rawData.length; ++i) {
+        outputArray[i] = rawData.charCodeAt(i);
+    }
+    return outputArray;
 }
--- service-worker.js
+++ service-worker.js
@@ -12,8 +12,6 @@ var urlsToCache = [
     "/pages/liga-indonesia.html",
     "/pages/liga-inggris.html",
     "/pages/liga-italia.html",
-    //  hapus /match karena ngga ada asset match.html
-    // "/pages/match.html",
     "/css/materialize.min.css",
     "/css/style.css",
     "/js/materialize.min.js",
@@ -42,22 +40,22 @@ var urlsToCache = [
 
 ];
 
-self.addEventListener("install", function (event) {
+self.addEventListener("install", function(event) {
     event.waitUntil(
-        caches.open(CACHE_NAME).then(function (cache) {
+        caches.open(CACHE_NAME).then(function(cache) {
             return cache.addAll(urlsToCache);
         })
     );
 });
 
 //  Menyimpan cache dinamis 
-self.addEventListener("fetch", function (event) {
+self.addEventListener("fetch", function(event) {
 
     var base_url = "https://api.football-data.org/v2/";
     if (event.request.url.indexOf(base_url) > -1) {
         event.respondWith(
-            caches.open(CACHE_NAME).then(function (cache) {
-                return fetch(event.request).then(function (response) {
+            caches.open(CACHE_NAME).then(function(cache) {
+                return fetch(event.request).then(function(response) {
                     cache.put(event.request.url, response.clone());
                     return response;
                 })
@@ -65,18 +63,18 @@ self.addEventListener("fetch", function (event) {
         );
     } else {
         event.respondWith(
-            caches.match(event.request, { ignoreSearch: true }).then(function (response) {
+            caches.match(event.request, { ignoreSearch: true }).then(function(response) {
                 return response || fetch(event.request);
             })
         )
     }
 });
 
-self.addEventListener("activate", function (event) {
+self.addEventListener("activate", function(event) {
     event.waitUntil(
-        caches.keys().then(function (cacheNames) {
+        caches.keys().then(function(cacheNames) {
             return Promise.all(
-                cacheNames.map(function (cacheName) {
+                cacheNames.map(function(cacheName) {
                     if (cacheName != CACHE_NAME) {
                         console.log("ServiceWorker: cache " + cacheName + " dihapus");
                         return caches.delete(cacheName);
